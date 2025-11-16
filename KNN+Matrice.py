f9929e48f911319f5cb095b9d4240759467e210b
Alan DELMES <163319358+AlanDadann@users.noreply.github.com>
2025-11-16 10:15:00 +0100

    Implement KNN evaluation metrics and output
    Added KNN predictions, accuracy, confusion matrix, and classification report.

--- KNN+Matrice.py
+++ KNN+Matrice.py
@@ -4,3 +4,18 @@ from sklearn.neighbors import KNeighborsClassifier
 
 knn = KNeighborsClassifier(n_neighbors=5)
 knn.fit(X_train, y_train)
+
+y_pred_knn = knn.predict(X_test)
+accuracy_knn = accuracy_score(y_test, y_pred_knn)
+conf_matrix_knn = confusion_matrix(y_test, y_pred_knn)
+class_report_knn = classification_report(y_test, y_pred_knn,zero_division=1)
+
+
+
+
+
+print(f"Accuracy of KNN: {accuracy_knn * 100:.2f}%")
+print("\nConfusion Matrix for KNN:")
+print(conf_matrix_knn)
+print("\nClassification Report for KNN:")
+print(class_report_knn)
